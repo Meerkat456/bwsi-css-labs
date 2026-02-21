d972dc52f71677e9dc4991ee4e80f66102eaedba
Meerkat456 <rainbowelephant65@gmail.com>
2026-02-21 15:09:45 -0600

    Update lab_1a.py
    Adding personal introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,21 @@ def main():
     name = "Meera Kalakkunnath" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! My name is Meera Kalakkunnath and I am a current freshman in Texas. I have a deep interest in everything related with math and science and participate in my school's FTC team. I have recently completed the CS50 Introduction to Comp Sci which has given me a pretty good understanding of many programming concepts (making me familiar with Python and the Github interface). Additionally, I was a student in the Columbia Pre College Quantum Mechanics course and I look forward to applying my knowledge of physics towards this project! Lastly, I have just participated in my school's district science fair (under the environmental engineering category) and I will be moving on to regionals which will take place next week.")
+
+    print("Here are some fun facts about me and hopefully we have some things in common!")
+
+    print("I am part of the school marching band as a percussionist.")
+
+    print("I play USTA tournaments and have a UTR of 4.5.")
+
+    print("I am play the electric and acoustic guitar (Hotel California is favorite for electric and Blackbird is favorite for acoustic).")
+
+    print("I am an LD varsity debater. Also, I am going to try congress (let me know if you have any tips)! ")
+
+    print("I am part of UNICEF and have a passion for community service and helping kids. ")
+
+    print("I love learning languages (B1 French, A1 Spanish, and Developing Japanese skills). ")
 
 if __name__ == "__main__":
     main()
